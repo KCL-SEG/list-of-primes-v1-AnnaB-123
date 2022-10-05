bb9f0b72a535668660743848369aad968f135fd2
Anna Brown <anna.2.brown@kcl.ac.uk>
2022-10-05 23:36:05 +0100

    Finish method primes

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+
+    count = 2
+    div = 2
+
+    while (len(list) < number_of_primes):
+
+        if (count % div == 0):
+            #checks if itself
+            if (count == div):
+                list.append(count)
+            count += 1
+            div = 2
+        else:
+            div += 1
+
     return list
